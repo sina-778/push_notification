6830cbf1cef94cb224ab52be34e101b1c88f036e
asmfahim1 <asmfahim1@gmail.com>
2022-07-25 14:55:32 +0600

    notification worked successfully

--- lib/main.dart
+++ lib/main.dart
@@ -14,22 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const NotificationApp(),
     );
   }
 }
-
 
 class NotificationApp extends StatefulWidget {
   const NotificationApp({Key? key}) : super(key: key);
@@ -39,7 +29,6 @@ class NotificationApp extends StatefulWidget {
 }
 
 class _NotificationAppState extends State<NotificationApp> {
-
   FlutterLocalNotificationsPlugin? localNotification;
   @override
   void initState() {
@@ -47,24 +36,27 @@ class _NotificationAppState extends State<NotificationApp> {
     super.initState();
     var androidInitialize = new AndroidInitializationSettings('ic_launcher');
     var iOSIntialize = new IOSInitializationSettings();
-    var initialzationSettings = new InitializationSettings(android: androidInitialize, iOS: iOSIntialize);
-    localNotification= new FlutterLocalNotificationsPlugin();
+    var initialzationSettings = new InitializationSettings(
+        android: androidInitialize, iOS: iOSIntialize);
+    localNotification = new FlutterLocalNotificationsPlugin();
     localNotification!.initialize(initialzationSettings);
-
-
   }
 
-  Future _showNotification() async{
-    var androidDetails = new AndroidNotificationDetails("channelId",
-        "Local Notification",channelDescription :"THis is Sina",playSound: true,priority: Priority.high,importance: Importance.high,);
-
-
+  Future _showNotification() async {
+    var androidDetails = new AndroidNotificationDetails(
+      "channelId",
+      "Local Notification",
+      channelDescription: "This is Sina from United Group",
+      playSound: true,
+      priority: Priority.high,
+      importance: Importance.high,
+    );
 
     var iosDetails = new IOSNotificationDetails();
-    var generalNotifications = new NotificationDetails(android: androidDetails,iOS: iosDetails);
-    final AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
-    await localNotification!.show(0, "Notification", "The Body of the Notification", generalNotifications);
-
+    var generalNotifications =
+        new NotificationDetails(android: androidDetails, iOS: iosDetails);
+    await localNotification!.show(0, "Notification",
+        "This is Sina from United Group", generalNotifications);
   }
 
   @override
@@ -80,4 +72,3 @@ class _NotificationAppState extends State<NotificationApp> {
     );
   }
 }
-
